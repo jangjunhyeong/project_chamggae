3a31404705a2964092a7f990b20ef97a10394886
jangjunhyeong <wkdwnsgud1@gmail.com>
2022-06-16 14:21:56 +0900

    media min,max-width로 breakpoint 구성

--- test/js/index.js
+++ test/js/index.js
@@ -1,2 +1,13 @@
 const root = document.getElementById("root")
 console.log(root);
+const button = document.getElementsByClassName("button");
+console.log(button);
+const search =document.getElementById("search");
+console.log(search);
+
+
+
+
+button[0].addEventListener("click",function(){
+	alert("hi");
+})
